d0404867323412cde33a2ad27ae077e948d12fc7
Paaka <Mateuszhartabus@gmail.com>
2019-12-16 09:29:50 +0100

    Initial commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={Styles.App}>
+      Weather App
+    </div>
+  );
+}
+
+export default App;
